90be0a09a29ef5d5cb4408a974ef5aa353c215a3
larryschirmer <larryschirmer@gmail.com>
2017-05-16 20:15:07 -0400

    make print grid method

--- A_Star.js
+++ A_Star.js
@@ -1,12 +1,17 @@
-let { log } = require('./logging');
+let { log, printGrid } = require('./logging');
 
 let makeGrid = ({ rows, cols }, node_obj) => {
-	return (arrayThing = Array.from(new Array(rows), _ => {
-		return Array.from(new Array(cols), _ => {
-			return new node_obj();
+	return (arrayThing = Array.from(new Array(rows), (u, i) => {
+		return Array.from(new Array(cols), (u, j) => {
+			let nodeObject = new node_obj();
+			nodeObject.i = i;
+			nodeObject.j = j;
+			return nodeObject;
 		});
 	}));
 };
+
+let openSet = [], closedSet = [], start, end;
 
 const cols = 5, rows = 5;
 
@@ -14,6 +19,8 @@ function Spot() {
 	this.f = 0;
 	this.g = 0;
 	this.h = 0;
+	this.i = 0;
+	this.j = 0;
 }
 
 let grid_opts = {
@@ -21,4 +28,15 @@ let grid_opts = {
 	cols: cols,
 };
 
-log(makeGrid(grid_opts, Spot));
+let grid = makeGrid(grid_opts, Spot);
+
+printGrid(grid);
+
+//log(grid);
+
+start = grid[0][0];
+end = grid[cols - 1][rows - 1];
+
+openSet = [...openSet, start];
+
+//log();
--- logging.js
+++ logging.js
@@ -4,6 +4,17 @@ let log = msg => {
 	console.log(util.inspect(msg, false, null));
 };
 
+let printGrid = grid => {
+	grid.forEach(row => {
+		let cols = '|';
+		for (let i = 0; i < row.length; i++) {
+			cols += ` ${row[i].i}, ${row[i].j} |`;
+		}
+		console.log(cols);
+	});
+};
+
 module.exports = {
 	log,
+	printGrid,
 };
