6375c4104f051902ed4eb629212dd91e964f18fc
larryschirmer <larryschirmer@gmail.com>
2017-05-26 10:41:31 -0400

    identify node neighbors

--- A_Star.js
+++ A_Star.js
@@ -4,14 +4,14 @@ let makeGrid = ({ rows, cols }, node_obj) => {
 	return (arrayThing = Array.from(new Array(rows), (u, i) => {
 		return Array.from(new Array(cols), (u, j) => {
 			let nodeObject = new node_obj();
-			nodeObject.i = i;
-			nodeObject.j = j;
+			nodeObject.x = i;
+			nodeObject.y = j;
+			nodeObject.rows = rows;
+			nodeObject.cols = cols;
 			return nodeObject;
 		});
 	}));
 };
-
-let openSet = [], closedSet = [], start, end;
 
 const cols = 5, rows = 5;
 
@@ -19,8 +19,25 @@ function Spot() {
 	this.f = 0;
 	this.g = 0;
 	this.h = 0;
-	this.i = 0;
-	this.j = 0;
+	this.x = 0;
+	this.y = 0;
+
+	this.rows = 0;
+	this.cols = 0;
+
+	this.set = 'X';
+	this.open = _ => {
+		this.set = 'O';
+	};
+	this.isNeighbor = false;
+	this.neighbors = [];
+	this.getNeighbors = grid => {
+		let r = this.x, c = this.y, rows = this.rows - 1, cols = this.cols - 1;
+		if (c < cols) this.neighbors.push(grid[r][c + 1]);
+		if (c > 0) this.neighbors.push(grid[r][c - 1]);
+		if (r < rows) this.neighbors.push(grid[r + 1][c]);
+		if (r > 0) this.neighbors.push(grid[r - 1][c]);
+	};
 }
 
 let grid_opts = {
@@ -33,10 +50,5 @@ let grid = makeGrid(grid_opts, Spot);
 printGrid(grid);
 
 //log(grid);
-
-start = grid[0][0];
-end = grid[cols - 1][rows - 1];
-
-openSet = [...openSet, start];
 
 //log();
--- logging.js
+++ logging.js
@@ -1,14 +1,41 @@
 const util = require('util');
 
+let row = index => {
+	return index - 1;
+};
+
+let col = index => {
+	return index - 1;
+};
+
 let log = msg => {
 	console.log(util.inspect(msg, false, null));
 };
 
+let showNeighbors = neighbors => {
+	neighbors.forEach(spot => {
+		spot.isNeighbor = true;
+	});
+};
+
+//end = grid[row(rows)][col(cols)];
+
+//openSet = [...openSet, start];
+
+let openSet = [], closedSet = [], start, end;
+
 let printGrid = grid => {
+	start = grid[row(3)][col(3)];
+	start.open();
+	start.getNeighbors(grid);
+	showNeighbors(start.neighbors);
 	grid.forEach(row => {
 		let cols = '|';
+
 		for (let i = 0; i < row.length; i++) {
-			cols += ` ${row[i].i}, ${row[i].j} |`;
+			let set = row[i].set;
+			if (row[i].isNeighbor) set = 'N';
+			cols += ` ${set} |`;
 		}
 		console.log(cols);
 	});
